dcd5484458ac45a9f919da3d2601cb09519a73f9
Alex Ray <alex@alex-laptop.(none)>
2009-10-24 03:04:58 -0400

    less bits

--- 7-ajr.py
+++ 7-ajr.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python
 #starts at 0,43; first is 5 wide; else are 7 wide
-import re
-from PIL import Image
-im = Image.open('oxygen.png')
-size = im.getbbox()
+import re, PIL.Image
+im = PIL.Image.open('oxygen.png')
 pix = im.load()
-lastcolor = None
 s = ''
-for i in range(0,size[2],7):
+for i in range(0,im.getbbox()[2],7):
     color = pix[i, 43]
     if color[0] == color[1] and color[1] == color[2]:
         s += chr(color[0])
-    lastcolor = color
 print ''.join(map(chr,eval(re.findall('(\[[^\]]*\])',s)[-1])))
