e3aeb58b1b1a38678e0e9faac523e58e632f4833
Michael Wright <mdwrigh2@ncsu.edu>
2009-10-23 23:59:57 -0400

    Updated solution to first problem

--- 1-mike.py
+++ 1-mike.py
@@ -5,4 +5,6 @@ intab = "abcdefghijklmnopqrstuvwxyz"
 outtab = "cdefghijklmnopqrstuvwxyzab"
 transtab = maketrans(intab,outtab)
 string = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+print string.translate(transtab)+"\n"
+string = "map"
 print string.translate(transtab)
