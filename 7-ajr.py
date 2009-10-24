decb56c221eb542521efa9718d92f5adb051505c
Alex Ray <alex@alex-laptop.(none)>
2009-10-24 03:39:27 -0400

    not mine

--- 7-ajr.py
+++ 7-ajr.py
@@ -1,3 +1,1 @@
-#!/usr/bin/env python
-#starts at 0,43; first is 5 wide; else are 7 wide, sampling every 7
 import PIL.Image;print ''.join(map(chr,eval(PIL.Image.open('oxygen.png').tostring()[108188:110620:28][42:])))
