5fe2e438b3da8ea0361bc9b7045ca57ba8e39b87
Michael Wright <mdwrigh2@ncsu.edu>
2009-10-23 23:58:30 -0400

    Added Mike's solution to problem 1

---
+++ 1-mike.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from string import maketrans
+
+intab = "abcdefghijklmnopqrstuvwxyz"
+outtab = "cdefghijklmnopqrstuvwxyzab"
+transtab = maketrans(intab,outtab)
+string = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+print string.translate(transtab)
