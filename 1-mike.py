d3983e66d48eb9541d7df1c6d1a7148837617772
Michael Wright <mdwrigh2@ncsu.edu>
2009-10-24 00:17:05 -0400

    Reading from stdin for problem 1 rather than from hardcoded values

--- 1-mike.py
+++ 1-mike.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 from string import maketrans
+from sys import stdin
 
-intab = "abcdefghijklmnopqrstuvwxyz"
-outtab = "cdefghijklmnopqrstuvwxyzab"
-transtab = maketrans(intab,outtab)
-string = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
-print string.translate(transtab)+"\n"
-string = "map"
-print string.translate(transtab)
+intab = str(raw_input("From: \n"))
+outtab = str(raw_input("To: \n"))
+string = str(raw_input("String: \n"))
+print "Translation: \n"+string.translate(maketrans(intab,outtab))
