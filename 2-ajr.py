94afbac090cc6f3f6d485d09d3a2e8ea0d4d25ae
Alex Ray <alex@alex-laptop.(none)>
2009-10-24 03:02:15 -0400

    7 ajr

--- 2-ajr.py
+++ 2-ajr.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from sys import stdin as s
+a = ''
 b = s.read()
 i = dict([(chr(i),0) for i in range(256)])
-for c in b: i[c] += 1
+for c in b: i[c] += 1; a += (c if ord(c) > 96 and ord(c) < 123 else '')
 for l in sorted(i.iteritems(), key=lambda (k,v): (v,k)): print l
+print a
