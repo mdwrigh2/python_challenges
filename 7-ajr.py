1dba5c3672bb06b1a1b4a37e7b7bcc47a88dd684
Alex Ray <alex@alex-laptop.(none)>
2009-10-24 03:17:34 -0400

    got rid of another line

--- 7-ajr.py
+++ 7-ajr.py
@@ -2,12 +2,4 @@
 #starts at 0,43; first is 5 wide; else are 7 wide, sampling every 7
 import re, PIL.Image
 im = PIL.Image.open('oxygen.png')
-pix = im.load()
-s = ''
-for i in range(0,im.getbbox()[2],7):
-    color = pix[i, 43]
-    if color[0] == color[1] and color[1] == color[2]:
-        s += chr(color[0])
-print ''.join(map(chr,eval(re.findall('(\[[^\]]*\])',s)[-1])))
-
-print ''.join(map(chr,eval(re.findall('(\[[^\]]*\])', ''.join([chr(pix[i, 43][0]) for i in range(0,im.getbbox()[2],7 if pix[i, 43][0] == pix[i, 43][1] == pix[i, 43][2]]))[-1])))
+print ''.join(map(chr,eval(re.findall('(\[[^\]]*\])', ''.join([chr(im.getpixel((i, 43))[0]) for i in range(0,im.getbbox()[2],7) if im.getpixel((i, 43))[0] == im.getpixel((i, 43))[1] == im.getpixel((i, 43))[2]]))[-1])))
